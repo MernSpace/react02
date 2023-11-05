7afe373d00683e887ace42398a523d1060632b5b
saharul sifat <126339980+SaharulSifatSoft@users.noreply.github.com>
2023-11-05 10:01:13 +0600

    ServicePage.jsx

--- src/assets/pages/ServicePage.jsx
+++ src/assets/pages/ServicePage.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import Header from '../component/Header';
-
+import Fotter from '../component/fotter'
 const ServicePage = () => {
     return (
         <div>
             <Header/>
+            <Fotter/>
         </div>
     );
 };
