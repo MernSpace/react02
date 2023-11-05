a7eafb95c75581658592f3016218d3368c0c160a
saharul sifat <126339980+SaharulSifatSoft@users.noreply.github.com>
2023-11-05 10:00:43 +0600

    ProjectPage.jsx

--- src/assets/pages/ProjectPage.jsx
+++ src/assets/pages/ProjectPage.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../component/Header';
+import Fotter from '../component/fotter'
 
 const ProjectPage = () => {
     return (
         <div>
             <Header/>
+            <Fotter/>
         </div>
     );
 };
